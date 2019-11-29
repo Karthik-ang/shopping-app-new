80f1e45b38af7652f66d903c1972350c1242796d
Karthik-ang <58217358+Karthik-ang@users.noreply.github.com>
2019-11-29 12:13:01 +0530

    localstorage implemented

--- src/modules/shoppingCart/cart.component.ts
+++ src/modules/shoppingCart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
+import { ShoppingCartFacade } from '../shopping-cart.facade';
 
 @Component({
     selector: 'cart',
@@ -7,18 +8,23 @@ import { Router } from '@angular/router';
 })
 export class CartComponent implements OnInit {
     title = 'shopping-app';
-    data = [];
     totalPrice = 0;
     isMobile = false;
     totalDiscount = 0;
+    isCartPage = true;
     // @Input() cartItems = [];
     // @Output() cartChange = new EventEmitter();
-    constructor(private router: Router) {
-        this.data = this.router.getCurrentNavigation().extras.state.example;
-        console.log(this.data, 'data');
+    constructor(private router: Router, public cartFacade: ShoppingCartFacade) {
     }
 
     ngOnInit() {
+        const items = JSON.parse(window.localStorage.getItem('cartItems'));
+        this.cartFacade.cartItems = items ? items : [];
+        if (!this.cartFacade.cartItems.length) {
+            this.router.navigate(['']);
+        } else {
+            this.cartFacade.findTotalItems();
+        }
         if (window.screen.width <= 768) {
             this.isMobile = true;
         }
@@ -28,7 +34,7 @@ export class CartComponent implements OnInit {
     calculateDiscount() {
         this.totalDiscount = 0;
         var self = this;
-        this.data.forEach(el => {
+        this.cartFacade.cartItems.forEach(el => {
             var discountPrice = el.discountValue * el.count;
             self.totalDiscount = self.totalDiscount + discountPrice;
         })
@@ -36,34 +42,42 @@ export class CartComponent implements OnInit {
     calculateTotal() {
         this.totalPrice = 0;
         var self = this;
-        this.data.forEach(el => {
+        this.cartFacade.cartItems.forEach(el => {
             var itemPrice = el.price * el.count;
             self.totalPrice = self.totalPrice + itemPrice;
-        })
+        });
     }
     changeCount(type, item) {
         if (type == 'dec') {
             item.count = item.count - 1;
+            if (item.count == 0) {
+                this.removeCartItem(item);
+            }
+
         } else {
             item.count = item.count + 1;
         }
+        window.localStorage.setItem('cartItems', JSON.stringify(this.cartFacade.cartItems));
         this.calculateTotal();
         this.calculateDiscount();
+        this.cartFacade.findTotalItems();
     }
     countChange(event, item) {
         if (event != '') {
             item.count = parseInt(event);
             this.calculateTotal();
             this.calculateDiscount();
+            this.cartFacade.findTotalItems();
         }
     }
     removeCartItem(item) {
-        const index = this.data.findIndex(x => x.id == item.id);
+        const index = this.cartFacade.cartItems.findIndex(x => x.id == item.id);
         if (index != -1) {
-            this.data.splice(index, 1);
-            if (this.data.length) {
+            this.cartFacade.cartItems.splice(index, 1);
+            if (this.cartFacade.cartItems.length) {
                 this.calculateTotal();
                 this.calculateDiscount();
+                this.cartFacade.findTotalItems();
             } else {
                 this.router.navigate(['']);
             }
